7f5b4426a4e54490fd87596200173af076dac6de
shinespark <shinespark64@gmail.com>
2016-12-19 18:20:21 +0900

    add test for users_login

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   get 'signup'  => 'users#new'
 
-  get    'login' => 'sessions#new'
-  post   'login' => 'sessions#create'
-  delete 'login' => 'sessions#destroy'
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
