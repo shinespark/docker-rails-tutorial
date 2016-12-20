6ea741749c34d7b32010a336553ddbe8f4db7484
shinespark <shinespark64@gmail.com>
2016-12-21 02:15:16 +0900

    finish chapter8

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   end
 
   def authenticated?(remember_token)
-    return false if remember_digest.nil
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
