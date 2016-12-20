8990701d301b3f49c7bf7a0c77d2377c5ba0713f
shinespark <shinespark64@gmail.com>
2016-12-20 16:20:05 +0900

    update sessions

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -9,8 +9,14 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  # ユーザーがログ人していればtrue, その他ならfalseを返す
+  # ユーザーがログインしていればtrue, その他ならfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+
+  # 現在のユーザーをログアウトする
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
