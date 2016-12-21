0935cfc33a01dd13910d266d2f31de92bd308870
shinespark <shinespark64@gmail.com>
2016-12-21 20:07:36 +0900

    finish 9.2

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -43,9 +44,17 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  # ログイン済みユーザーかどうか確認
   def logged_in_user
     return if logged_in?
+    store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
