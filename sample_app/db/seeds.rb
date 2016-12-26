e90fd3fe60f1c6cf065937f2fd62adf9e1b9e08b
shinespark <shinespark64@gmail.com>
2016-12-26 13:52:36 +0900

    add admin

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  'Example User',
+             email: 'example@railstutorial.org',
+             password:              'foobar',
+             password_confirmation: 'foobar',
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
-  password = "password"
+  password = 'password'
   User.create!(name:  name,
                email: email,
                password:              password,
