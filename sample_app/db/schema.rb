125af7c5552107ed28c183a7b26f7b4251337ba6
shinespark <shinespark64@gmail.com>
2016-12-26 18:11:56 +0900

    ignore test error

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226054503) do
+ActiveRecord::Schema.define(version: 20161226072719) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",              limit: 255
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161226054503) do
     t.string   "activation_digest", limit: 255
     t.boolean  "activated",                     default: false
     t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
