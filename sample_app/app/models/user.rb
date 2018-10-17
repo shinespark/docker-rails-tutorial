e6cbe599a4c2b022d9894a4118668221d8643176
shinespark <shinespark64@gmail.com>
2018-10-17 14:15:03 +0900

    Add user following

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -77,9 +77,17 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where('user_id = ?', id)
+    # Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+    #                 following_ids: following_ids, user_id: id)
+
+    # サブセレクトを利用することでDBのキャッシュ機構を利用するパターン。
+    # ここでは、relatiossihpsテーブルのselectがキャッシュ化される
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # ユーザーをフォローする
